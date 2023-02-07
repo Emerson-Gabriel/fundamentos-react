bc9d609adfadac5b5c6031e6b3c28b9c00bccc4c
Emerson Gabriel Pena Fernandes <emersong730@gmail.com>
2023-02-07 13:37:19 -0300

    novas atualizações do curso de react

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
+import './app.css'
 import React from "react";
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from "./components/basicos/Aleatorio";
+import Card from "./components/layout/Card";
 
 export default _ =>
-    <div id="app">
-        <h1>Fundamentos React</h1>
-        <Aleatorio 
-            max={97} 
-            min={13} 
-        />
-        <Fragmento />
-        <ComParametro
-            titulo="Informações do aluno:"
-            aluno="Emerson"
-            nota={9.8}
-        />
-        <Primeiro></Primeiro>
+    <div className="app">
+        <h1 className="center">Fundamentos React</h1>
+        <div className="cards">
+            <Card titulo="#04 - Desafio Aleatório">
+                <Aleatorio  max={97}  min={13} />
+            </Card>
+            <Card titulo="#03 - Fragmento">
+                <Fragmento />
+            </Card>
+            <Card titulo="#02 - Com Parâmetro">
+                <ComParametro
+                    titulo="Informações do aluno:"
+                    aluno="Emerson"
+                    nota={9.8}
+                />
+            </Card>
+            <Card titulo="#01 - Primeiro Componente">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
     </div>
--- src/components/basicos/Aleatorio.jsx
+++ src/components/basicos/Aleatorio.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 export default (props) => {
+    /* exemplo de atribuição de valores com js */
+    const [a, b] = [1, 2]
+    const {c, d, x} = {c: 54, d: 421, x: 'testes'}
+
     return (
-        Math.ceil(Math.random() * (props.max - props.min) + props.min)
+        <React.Fragment>
+            <h2>Valor Aleatório</h2>
+            <p>
+                Valor mínimo: {props.min}
+            </p>
+            <p>
+                Valor máximo: {props.max}
+            </p>
+            <p>
+                Valor aleatório: {Math.ceil(Math.random() * (props.max - props.min) + props.min)}
+            </p>
+        </React.Fragment>
     )
 }
