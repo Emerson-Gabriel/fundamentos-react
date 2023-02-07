26623734d8190d822c3d6dbacaa5001082b4ddf6
Emerson Gabriel Pena Fernandes <emersong730@gmail.com>
2023-02-06 21:40:51 -0300

    fazendo componente de numero aleatório

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
+import Aleatorio from "./components/basicos/Aleatorio";
 
 export default _ =>
     <div id="app">
         <h1>Fundamentos React</h1>
+        <Aleatorio 
+            max={97} 
+            min={13} 
+        />
         <Fragmento />
         <ComParametro
             titulo="Informações do aluno:"
--- src/components/basicos/Aleatorio.jsx
+++ src/components/basicos/Aleatorio.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 export default (props) => {
-    
     return (
-
+        Math.ceil(Math.random() * (props.max - props.min) + props.min)
     )
 }
