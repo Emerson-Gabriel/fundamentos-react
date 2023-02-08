acf27088561f463cd38c32007d371bbd3c900d66
Emerson Gabriel Pena Fernandes <emersong730@gmail.com>
2023-02-07 22:52:34 -0300

    demais mudanças no projeto de react

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,20 @@ export default _ =>
     <div className="app">
         <h1 className="center">Fundamentos React</h1>
         <div className="cards">
-            <Card titulo="#04 - Desafio Aleatório">
+            <Card titulo="#04 - Desafio Aleatório" color="#fa6900">
                 <Aleatorio  max={97}  min={13} />
             </Card>
-            <Card titulo="#03 - Fragmento">
+            <Card titulo="#03 - Fragmento" color="#e94c6f">
                 <Fragmento />
             </Card>
-            <Card titulo="#02 - Com Parâmetro">
+            <Card titulo="#02 - Com Parâmetro" color="#e8b71a">
                 <ComParametro
                     titulo="Informações do aluno:"
                     aluno="Emerson"
                     nota={9.8}
                 />
             </Card>
-            <Card titulo="#01 - Primeiro Componente">
+            <Card titulo="#01 - Primeiro Componente" color="#588c73">
                 <Primeiro></Primeiro>
             </Card>
         </div>
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -2,9 +2,15 @@ import './Card.css';
 import React from "react";
 
 export default props => {
+    const style1 = {
+        backgroundColor: props.color || 'red'
+    }
+    const style2 = {
+        borderColor: props.color || 'red'
+    }
     return (
-        <div className='card'>
-            <div className='title'>
+        <div className='card' style={style2}>
+            <div className='title' style={style1}>
                 {props.titulo}
             </div>
             <div className='content'>
